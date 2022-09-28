32efca9bb00b2779e40f29c84acd835165c769c9
David Reese <davidreese5544@gmail.com>
2022-09-28 09:46:14 -0600

    navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAJh0GQJZr1iUmo2f9dZGoMsstoV42KJQ",
+    authDomain: "clone-1a80b.firebaseapp.com",
+    projectId: "clone-1a80b",
+    storageBucket: "clone-1a80b.appspot.com",
+    messagingSenderId: "901093481858",
+    appId: "1:901093481858:web:735c786a555e0b3afe039b",
+    measurementId: "G-W1FS3CJ07Y"
+  };
