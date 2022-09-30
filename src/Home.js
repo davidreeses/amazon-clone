1032cb1ed140205e13bfe025561a2ad8fe0687d1
David Reese <davidreese5544@gmail.com>
2022-09-30 11:27:11 -0600

    adding checkout page and react router

--- src/Home.js
+++ src/Home.js
@@ -13,17 +13,56 @@ function Home() {
         />
 
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product 
+           id="123321341"
+           title="The lean startup: How Constant Innovation Creates Succesful Businesses - paperback" price={29.99}
+           image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" rating={5}
+           />
+          
+          <Product 
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough hook and whisk, 5 liter Glass Bowl"
+            price={239.00}
+            rating={4}
+            image="https://www.kenwoodworld.com/WebImage/Global/Product%20images/Kmix%20products/KMX754RD/KMX754RD_Picasso-KM-Glass-Bowl-BMK_V2_800x600.png"
+          />
         </div>
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+            <Product
+             id="490380"
+             title="Samsung Galaxy Watch 5 44mm Bluetooth Smartwatch"
+             price={309.99}
+             rating={4}
+             image="https://m.media-amazon.com/images/I/61s1Na3W61L._AC_SX679_.jpg"
+            />
+
+            <Product
+             id="23445930"
+             title="Amazon Echo (3rd generation | Smart speaker with Alexa, Charcoal Fabric"
+             price="98.99"
+             rating={5}
+             image="https://i.ebayimg.com/images/g/KJMAAOSwhwBZ4yQq/s-l640.jpg" 
+            />
+
+            <Product
+             id="3254354345"
+             title="New Apple Ipad Pro (12.9-inch, Wi-Fi, 123GB) - Silver (4th Generation)"
+             price={598.99}
+             rating={4}
+             image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-ipad-pro-12-wifi-silver-2020?wid=2000&hei=2000&fmt=jpeg&qlt=90&.v=1626721066000"
+            />
         </div>
 
-        <div className="home__row">{/* Product */}</div>
+        <div className="home__row">
+            <Product
+            id="490380"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6360/6360752_sd.jpg"
+             />
+            </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({id, title, image, price, rating}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="prouct__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐️</p>
+          {Array(rating).fill().map((_,i) => (
+               <p>⭐️</p>
+          ))}
         </div>
       </div>
 
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
-        alt="the lean startup cover"
+        src={image}
+        alt=""
       />
 
-      <button>Add to Basket</button>
+      <button>Add to Cart</button>
     </div>
   );
 }
